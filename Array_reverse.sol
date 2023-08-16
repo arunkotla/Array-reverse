70a75decbc2e1eb065007e1735def5045afbe132
arunkotla <arunkkotla1998@gmail.com>
2023-08-17 01:56:14 +0530

    Documentation

--- Array_reverse.sol
+++ Array_reverse.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 contract reversearray {
 
+  // adding a function to create a reversed array
   function revArr(uint[] memory givenArr) public pure returns(uint[] memory){
     uint[] memory Newarr = new uint[](givenArr.length);
     
+    //creating new reversed array
     for (uint i=0; i < givenArr.length ; i++){
       Newarr[i] = givenArr[givenArr.length -1 -i];
     }
